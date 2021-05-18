95c5f188b9b93848d5e2e0f2c80e9f3b2914847d
davelee212 <dave@davesdomain.co.uk>
2021-05-18 23:07:11 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-variable "management_group_policy" {
+variable "target_management_group_name" {
     type = string
     description = "Name of the management group to create Azure Policy Defintions"
 }
