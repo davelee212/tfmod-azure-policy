df9d2a99acb44c8ea2ce20b68e74ee20132d0e4c
davelee212 <dave@davesdomain.co.uk>
2021-05-18 23:20:06 +0100

    Update builtin-allowed-locations.tf

--- builtin-allowed-locations.tf
+++ builtin-allowed-locations.tf
@@ -7,7 +7,7 @@ variable "allowedLocations" {
 
 resource "azurerm_policy_assignment" "allowed-locations" {
   name                 = "allowed-locations"
-  scope                = data.azurerm_management_group.target_management_group.id
+  scope                = data.azurerm_management_group.target_management_group_name.id
   policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/e56962a6-4747-49cd-b67b-bf8b01975c4c"
   display_name         = "Allowed locations"
 
