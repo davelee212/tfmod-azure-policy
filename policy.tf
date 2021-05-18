6db828a8a835e89af188584dd8108bfaa18f3a6a
davelee212 <dave@davesdomain.co.uk>
2021-05-18 23:02:32 +0100

    Create policy.tf

---
+++ policy.tf
@@ -0,0 +1,36 @@
+
+
+
+# Enforce Policies
+# "[Preview]: Deploy Log Analytics agent for Linux virtual machine scale sets"
+# "[Preview]: Deploy Log Analytics agent for Windows virtual machine scale sets"
+# "Deploy default Microsoft IaaSAntimalware extension for Windows Server"
+# "[Preview]: Deploy Log Analytics agent for Windows VMs",
+# "[Preview]: Deploy Log Analytics agent for Linux VMs",
+# "Deploy SQL DB transparent data encryption",
+# "Deploy Threat Detection on SQL servers"
+# "Allowed locations"
+# "Allowed locations for resource groups"
+# "Deploy network watcher when virtual networks are created"
+# "Allowed storage account SKUs"
+# "Allowed virtual machine SKUs"
+# "Deploy Dependency agent for Windows VMs"
+
+# Audit Policies
+#"Require automatic OS image patching on Virtual Machine Scale Sets"
+#"Audit diagnostic setting"
+#"Deprecated accounts should be removed from your subscription"
+#"Vulnerability assessment should be enabled on your SQL servers"
+#"Audit VMs that do not use managed disks"
+#"Azure Backup should be enabled for Virtual Machines"
+#"Vulnerability assessment should be enabled on your SQL managed instances"
+#"Vulnerabilities on your SQL databases should be remediated"
+#"Azure VPN gateways should not use 'basic' SKU"
+#"Deprecated accounts with owner permissions should be removed from your subscription"
+#"FTPS should be required in your Web App"
+#"Function App should only be accessible over HTTPS"
+#"Endpoint protection solution should be installed on virtual machine scale sets"
+#"Diagnostic logs in Virtual Machine Scale Sets should be enabled"
+
+
+
