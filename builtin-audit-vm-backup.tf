d2e88aa0bd0cf0bef6ed2f156276da6500e15c5c
davelee212 <dave@davesdomain.co.uk>
2021-05-18 23:25:22 +0100

    Update builtin-audit-vm-backup.tf

--- builtin-audit-vm-backup.tf
+++ builtin-audit-vm-backup.tf
@@ -1,6 +1,6 @@
 resource "azurerm_policy_assignment" "audit-vm-backup" {
   name                 = "audit-vm-backup"
-  scope                = data.azurerm_management_group.target_management_group.id
+  scope                = data.azurerm_management_group.target_management_group_name.id
   policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/013e242c-8828-4970-87b3-ab247555486d"
   display_name         = "Azure Backup should be enabled for Virtual Machines"
 
